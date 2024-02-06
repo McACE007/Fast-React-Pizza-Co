2e5b2c88bbdac3dfe1ce199dc50f625c8528eb93
mcace007 <mrinalace@gmail.com>
2024-02-06 18:53:47 +0530

    Update: Reading and Updating the User State

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,16 +1,26 @@
+import { useSelector } from "react-redux";
 import CreateUser from "../features/user/CreateUser";
+import Button from "./Button";
 
 function Home() {
+  const username = useSelector((store) => store.user.username);
+
   return (
     <div className="my-10 px-4 text-center sm:my-10">
-      <h1 className="text-xl font-semibold mb-8 md:text-3xl">
+      <h1 className="mb-8 text-xl font-semibold md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser />
+      {username === "" ? (
+        <CreateUser />
+      ) : (
+        <Button type="primary" to="/menu">
+          Continue ordering, {username}
+        </Button>
+      )}
     </div>
   );
 }
